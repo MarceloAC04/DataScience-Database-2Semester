09d8fe1b24589063497ba7b735eca35ab8a2528c
Marcelo <marcelocardoso020304@gmail.com>
2025-09-04 21:39:05 -0300

    Continuação bancos híbridos

--- empresa_xpto.sql
+++ empresa_xpto.sql
@@ -24,3 +24,46 @@ VALUES
 
 SELECT nome , email FROM clientes
 WHERE JSON_CONTAINS(preferencias ->'$.interesses', '"livros"');
+
+SELECT JSON_VALID('{"interesses": ["livros", "computação"], 
+"notificacoes": true
+}');
+
+SELECT nome FROM clientes
+WHERE JSON_EXTRACT(preferencias -> '$.notificacoes') = false;
+
+SELECT nome, JSON_EXTRACT(preferencias,'$.interesses') as interesses
+FROM clientes
+WHERE JSON_EXTRACT(preferencias,'$.notificacoes') = false;
+
+SELECT nome, JSON_EXTRACT(preferencias,'$.interesses[0]') as interesses
+FROM clientes
+WHERE JSON_EXTRACT(preferencias,'$.notificacoes') = false;
+
+SELECT nome, JSON_KEYS(preferencias) FROM clientes;
+SELECT JSON_TYPE(preferencias->'$.interesses[0]') FROM clientes;
+
+INSERT INTO clientes (nome, email, preferencias)
+VALUES
+('Viniccius', 'vini@email.com', 
+'{}');
+
+UPDATE clientes SET preferencias = JSON_INSERT(preferencias, '$.premium', true)
+ WHERE id=3; -- Criar campo dentro do documento 
+ -- JSON_SET( -- Atualiza um campo
+ -- JSON_REMOVE(preferencias, '$.premium') -- remove um campo do documento
+ SELECT * FROM clientes;
+ 
+ 
+ -- Remove as Aspas
+SELECT nome, 
+JSON_UNQUOTE(preferencias ->'$.interesses[0]')as interesses1, 
+JSON_UNQUOTE(preferencias ->'$.interesses[1]') as interesses2,
+JSON_UNQUOTE(preferencias ->'$.interesses[2]') as interesses3
+FROM clientes;
+
+SELECT nome, 
+preferencias ->>'$.interesses[0]'as interesses1,
+preferencias ->>'$.interesses[1]' as interesses2,
+preferencias ->>'$.interesses[2]'  as interesses3
+FROM clientes;
